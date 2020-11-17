b7d9c4fb08ee336563366b752e40f63d98837967
Jordan Assayah <jassayah@makor-capital.com>
2020-11-17 09:54:12 +0200

    Remove unecessary timeout

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const axios = require('axios')
 const WebSocket = require('ws')
 const server = require('http').createServer()
 const wss = new WebSocket.Server({ server, port: 3000 })
-const decrypt = require('./helpers/AesBase64Crypto')
 
 wss.on('connection', async (ws, req) => {
   console.log('Someone connected')
@@ -10,51 +9,45 @@ wss.on('connection', async (ws, req) => {
   const urlJsObj = JSON.parse(decodeURI(parsedURL))
   const resp = await axios.get('http://auth-provider.gamy-tech.com/accounts/' + urlJsObj.ClientId)
 
-  setTimeout(() => {
-    const socketToJSP = new WebSocket('ws://srv0.gamy-tech.com:8080/GamyTechServer2.2B/game/' + parsedURL)
-    socketToJSP.on('message', msg => {
-      // const decodedMsg = decrypt(msg)
-      // const convertedToJS = JSON.parse(decodedMsg)
+  const socketToJSP = new WebSocket('ws://srv0.gamy-tech.com:8080/GamyTechServer2.2B/game/' + parsedURL)
+  socketToJSP.on('message', msg => ws.send(msg))
+
+  let XYZCompanyWebSocket
+  if (urlJsObj.CliendId !== 'gamytech-client-id') {
+    XYZCompanyWebSocket = new WebSocket(resp.data.websocketUrl)
+    XYZCompanyWebSocket.on('message', msg => {
+      console.log('XYZCompanyWebSocket -> :', msg)
       ws.send(msg)
     })
-
-    let XYZCompanyWebSocket
-    if (urlJsObj.CliendId !== 'gamytech-client-id') {
-      XYZCompanyWebSocket = new WebSocket(resp.data.websocketUrl)
-      XYZCompanyWebSocket.on('message', msg => {
-        console.log('XYZCompanyWebSocket -> :', msg)
-        ws.send(msg)
-      })
-    }
+  }
   
-    ws.on('message', msg => {
-      const convertedToJS = JSON.parse(msg)
+  ws.on('message', msg => {
+    const convertedToJS = JSON.parse(msg)
 
-      if (urlJsObj.ClientId !== 'gamytech-client-id') {
-        switch (convertedToJS.Service) {
-          case 'CashIn':
-          case 'CashInApco':
-          case 'CashOutRequest':
-          case 'Login':
-          case 'Logout':
-          case 'PurchaseItem':
-          case 'RegisterUser':
-          // case 'ApiRequest':
-          // case 'ApiUpdate':
-          // case 'DeleteCard':
-          // case 'ForgotPassword':
-          // case 'ResetPassword':
-            XYZCompanyWebSocket.send(msg)
-            break;
-          default:
-            socketToJSP.send(msg)
-            break;
-        }
-      } else {
-        socketToJSP.send(msg)
+    if (urlJsObj.ClientId !== 'gamytech-client-id') {
+      switch (convertedToJS.Service) {
+        case 'CashIn':
+        case 'CashInApco':
+        case 'CashOutRequest':
+        case 'Login':
+        case 'Logout':
+        case 'PurchaseItem':
+        case 'RegisterUser':
+        // case 'ApiRequest':
+        // case 'ApiUpdate':
+        // case 'DeleteCard':
+        // case 'ForgotPassword':
+        // case 'ResetPassword':
+          XYZCompanyWebSocket.send(msg)
+          break;
+        default:
+          socketToJSP.send(msg)
+          break;
       }
-    })
-  }, 1000)
+    } else {
+      socketToJSP.send(msg)
+    }
+  })
 })
 
 
