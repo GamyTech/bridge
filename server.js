d789df9fbc3b2c9c653497be9e61ae003c23e0b5
Jordan Assayah <jassayah@makor-capital.com>
2020-12-10 16:22:51 +0200

    Use external api in REST instead of websocket

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ const axios = require('axios')
 const WebSocket = require('ws')
 const server = require('http').createServer()
 const wss = new WebSocket.Server({ server, port: 3000 })
+const axios = require('axios')
+
+const getUserWithToken = async (externalApiUrl, token) => {
+  try {
+    const response = await axios.get(externalApiUrl + '/get-user-with-token', null, {
+      headers: {
+        Authorization: 'Bearer ' + token
+      }
+    })
+    if (response.status === 200) {
+      return response.data
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 wss.on('connection', async (ws, req) => {
   console.log('Someone connected')
@@ -11,28 +27,26 @@ wss.on('connection', async (ws, req) => {
 
   const socketToJSP = new WebSocket('ws://srv0.gamy-tech.com:8080/GamyTechServer2.2B/game/' + parsedURL)
   socketToJSP.on('message', msg => ws.send(msg))
-
-  let XYZCompanyWebSocket
-  if (urlJsObj.CliendId !== 'gamytech-client-id') {
-    XYZCompanyWebSocket = new WebSocket(resp.data.websocketUrl)
-    XYZCompanyWebSocket.on('message', msg => ws.send(msg))
-  }
   
-  ws.on('message', msg => {
+  ws.on('message', async msg => {
     const convertedToJS = JSON.parse(msg)
+    console.log(convertedToJS)
 
     if (urlJsObj.ClientId !== 'gamytech-client-id') {
       switch (convertedToJS.Service) {
-        case 'Login':
-        case 'Logout':
-        case 'RegisterUser':
-        case 'ForgotPassword':
-        case 'ResetPassword':
-        case 'CashIn':
-        case 'CashInSkrill':
-        case 'CashOutRequest':
-          XYZCompanyWebSocket.send(msg)
-          break;
+        case 'Login': {
+          if (urlJsObj.Token !== undefined) {
+            const user = await getUserWithToken(resp.data.externalApiUrl, urlJsObj.Token)
+
+            socketToJSP.send(JSON.stringify({
+              Service: 'Login',
+              Email: user.email,
+              UserName: user.user_name,
+              ClientId: urlJsObj.ClientId,
+              DeviceId: urlJsObj.DeviceId
+            }))
+          }
+        }
         default:
           socketToJSP.send(msg)
           break;
